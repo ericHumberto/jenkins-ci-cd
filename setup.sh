70e0071878bf3b4e9f6e27070dee399797817694
jhonnymoreno-mbciet <jhonny.moreno@ext.mercadobitcoin.com.br>
2022-04-07 10:49:05 -0300

    Adding Trivy instalation

--- setup.sh
+++ setup.sh
@@ -13,3 +13,8 @@ sudo systemctl restart jenkins
 git clone https://github.com/ZupIT/horusec-platform.git
 cd horusec-platform/
 make install
+sudo apt-get install wget apt-transport-https gnupg lsb-release
+wget -qO - https://aquasecurity.github.io/trivy-repo/deb/public.key | sudo apt-key add -
+echo deb https://aquasecurity.github.io/trivy-repo/deb $(lsb_release -sc) main | sudo tee -a /etc/apt/sources.list.d/trivy.list
+sudo apt-get update
+sudo apt-get install trivy
